d2f976ddd0c32e0bb066c04b7042a99c2a9304d8
JacobZwang <59858341+JacobZwang@users.noreply.github.com>
2022-08-07 14:25:58 -0700

    add creator & why section

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -15,10 +15,16 @@
 </svelte:head>
 
 <div class="grid md:grid-cols-2 gap-4 p-8">
-	<div class="col-span-full">
-		<h1 class="text-3xl">Sortable Action</h1>
+	<div>
+		<h1 class="text-3xl">Svelte Sortable Action</h1>
 
 		<p>Minimal Svelte action to add drag and drop sorting to a list.</p>
+	</div>
+
+	<div class="flex justify-end">
+		<p>
+			By <a class="text-blue-500" href="https://zwa.ng" target="_blank">Jacob Zwang</a>
+		</p>
 	</div>
 
 	<div class="border-solid border-2 rounded-md overflow-scroll">
@@ -35,5 +41,19 @@
 				{item}
 			</div>
 		{/each}
+	</div>
+
+	<div>
+		<h2 class="text-2xl">Why?</h2>
+		Existing libraries such as
+		<a
+			class="text-blue-500"
+			href="https://github.com/brunomolteni/svelte-sortable-list"
+			target="_blank"
+		>
+			svelte-sortable-list</a
+		> use extra dom nodes and require resturcturing your markup. svelte-action-sortable only uses 1 single
+		dom node to indicate where the item will be moved to and does not require you to change your markup
+		at all.
 	</div>
 </div>
